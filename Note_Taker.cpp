673e0f0c4ccfba9c75a99e15888a5f9b246af207
Lewis <118134324+Lewismwaz@users.noreply.github.com>
2023-05-02 15:45:54 +0300

    Update Note_Taker.cpp

--- Note_Taker.cpp
+++ Note_Taker.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 using namespace std;
+
+const string PROMPT_FILENAME = "Enter the name of the text file to be saved (e.g. readme.txt): ";
+const string PROMPT_TEXT = "Enter your text (type 'exit' to save and quit): ";
+const string ERROR_FILENAME = "Error: Invalid filename. Please enter a valid filename without spaces or special characters.";
+const string ERROR_OPEN_FILE = "Error: Failed to create file.";
 
 int main()
 {
     string filename, line;
-    cout << "Enter the name of the text file to be saved e.g. (readme.txt):";
+    cout << PROMPT_FILENAME;
     getline(cin, filename);
 
-    ofstream file(filename);
-    if (!file) {
-        cout << "Error creating the text file!" << endl;
+    // Validate filename
+    if (!regex_match(filename, regex("^[a-zA-Z0-9_\\.]+$"))) {
+        cout << ERROR_FILENAME << endl;
         return 1;
     }
 
-    cout << "1. Enter your text:" << endl;
-    cout << "2. Finished? Press ENTER, then type 'exit' to save and quit.):" << endl;
-    while (getline(cin, line)) {
-        if (line == "exit") {
-            break;
-        }
-        file << line << endl;
+    ofstream file(filename);
+    if (!file) {
+        cout << ERROR_OPEN_FILE << endl;
+        return 1;
     }
+
+    cout << PROMPT_TEXT;
+    do {
+        getline(cin, line);
+        if (line != "exit") {
+            file << line << endl;
+        }
+    } while (line != "exit");
 
     file.close();
     cout << "File saved in current directory. Exiting..." << endl;
